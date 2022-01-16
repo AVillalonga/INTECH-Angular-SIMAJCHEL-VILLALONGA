f5b9f1bb6e18166048f602cd414b73ac2dbf9136
Adrien Villalonga <adrien.villalonga@gmail.com>
2022-01-16 12:39:24 +0100

    Sign In

--- iti-community/src/modules/authentication/authentication.module.ts
+++ iti-community/src/modules/authentication/authentication.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { AuthenticationStore } from './authentication.store';
 import { AuthenticationStorage } from './authentication.storage';
 import { LoginComponent } from './components/login/login.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { AuthenticationCommands } from './services/authentication.commands';
@@ -37,7 +37,8 @@ import { AuthenticationInterceptor } from './authentication.interceptor';
     FormsModule,
     NzFormModule,
     NzButtonModule,
-    NzMessageModule
+    NzMessageModule,
+    ReactiveFormsModule,
   ]
 })
 export class AuthenticationModule { }
--- iti-community/src/modules/authentication/components/login/login.component.ts
+++ iti-community/src/modules/authentication/components/login/login.component.ts
@@ -1,13 +1,8 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { NzMessageService } from "ng-zorro-antd/message";
 import { AuthenticationService } from '../../services/authentication.service';
-
-class LoginFormModel {
-  username = "";
-  password = "";
-}
 
 @Component({
   selector: 'app-login',
@@ -15,39 +10,49 @@ class LoginFormModel {
   styleUrls: ['./login.component.less']
 })
 export class LoginComponent implements OnInit {
-  @ViewChild(NgForm, { static: false })
-  ngForm: NgForm;
 
-  model = new LoginFormModel();
+  loginForm: FormGroup;
 
   constructor(
     private router: Router,
     private authService: AuthenticationService,
-    private nzMessageService: NzMessageService
+    private nzMessageService: NzMessageService,
+    private fb: FormBuilder,
   ) { }
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      error: ['']
+    });
   }
 
-  goToRegistration() {
-    // TODO naviguer vers "/splash/register"
-  }
+  goToRegistration(): void { this.router.navigate(["/splash/register"]); }
 
   submit() {
     this.login();
   }
 
   async login() {
-    if (this.ngForm.form.invalid) {
-      return;
-    }
+    if (this.loginForm.valid) {
+      try {
+        const response = await this.authService.authenticate(
+          this.loginForm.controls.username.value,
+          this.loginForm.controls.password.value
+        );
 
-    try {
-      // TODO vérifier le résultat de l'authentification. Rediriger sur "/" en cas de succès ou afficher une erreur en cas d'échec
-      await this.authService.authenticate(this.model.username, this.model.password);
+        if (response) {
+          response.success
+            ? await this.router.navigate(['/'])
+            : this.nzMessageService.error("Nom d'utilisateur ou mot de passe incorrect");
+        } else {
+          this.nzMessageService.error("Une erreur est survenue. Veuillez réessayer plus tard");
+        }
 
-    } catch (e) {
-      this.nzMessageService.error("Une erreur est survenue. Veuillez réessayer plus tard");
+      } catch (exception) {
+        this.nzMessageService.error("Une erreur est survenue. Veuillez réessayer plus tard");
+      }
     }
   }
 }

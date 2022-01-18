304e1e7e5044bba1015be337d2a83728dbbde5eb
AVillalonga <adrien.villalonga@gmail.com>
2022-01-18 17:22:38 +0100

    Add author informations

---
+++ iti-community/src/modules/feed/pipe/feed-date.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FeedDatePipe } from './feed-date.pipe';
+
+describe('FeedDatePipe', () => {
+  it('create an instance', () => {
+    const pipe = new FeedDatePipe();
+    expect(pipe).toBeTruthy();
+  });
+});

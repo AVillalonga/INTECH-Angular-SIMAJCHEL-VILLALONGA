cd89428c2a8272bffce53c6371ff6915fa63e03c
AVillalonga <adrien.villalonga@gmail.com>
2022-01-18 14:50:23 +0100

    Room menu & FeedBackService

--- iti-community/src/modules/room/components/room-create-modal/room-create-modal.component.ts
+++ iti-community/src/modules/room/components/room-create-modal/room-create-modal.component.ts
@@ -1,12 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { RoomType } from '../../room.model';
 import { RoomService } from '../../services/room.service';
-
-export class CreateRoomFormModel {
-  name: string = "";
-  type: RoomType = RoomType.Text;
-}
 
 @Component({
   selector: 'app-room-create-modal',
@@ -14,36 +9,34 @@ export class CreateRoomFormModel {
   styleUrls: ['./room-create-modal.component.less']
 })
 export class RoomCreateModalComponent implements OnInit {
-  @ViewChild("f")
-  form: NgForm;
 
+  roomCreationForm: FormGroup;
   isVisible: boolean = false;
-  model = new CreateRoomFormModel();
 
-  constructor(private roomService: RoomService) {
-
-  }
+  constructor(
+    private roomService: RoomService,
+    private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.roomCreationForm = this.fb.group({
+      name: ['', [Validators.required]],
+      type: ['', [Validators.required]]
+    })
   }
 
-  async onOk() {
-    if (this.form.form.valid) {
-      // TODO invoquer la méthode create du RoomService
+  async submit() {
+    if (this.roomCreationForm.valid) {
+      this.roomService.create(this.roomCreationForm.controls.name.value, this.roomCreationForm.controls.type.value);
       this.close();
+    } else {
+      for (const key in this.roomCreationForm.controls) {
+        const formElement = this.roomCreationForm.get(key);
+        if (formElement instanceof FormControl) formElement.updateValueAndValidity();
+        /* @TODO: FIX (Show error onsubmit()) */
+      }
     }
   }
 
-  onCancel() {
-    this.close();
-  }
-
-  open() {
-    this.form.resetForm(new CreateRoomFormModel());
-    this.isVisible = true;
-  }
-
-  close() {
-    this.isVisible = false;
-  }
+  open() { this.isVisible = true; }
+  close() { this.isVisible = false; }
 }

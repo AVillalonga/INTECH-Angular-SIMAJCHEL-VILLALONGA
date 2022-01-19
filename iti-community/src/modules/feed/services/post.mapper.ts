48e744a5c6a843b061a1be856d4072059c99411c
reybun <yann.minuit@yahoo.fr>
2022-01-19 14:59:40 +0100

    fixed regex once again :tiger:
    :rat: TEMA LA TAILLE

--- iti-community/src/modules/feed/services/post.mapper.ts
+++ iti-community/src/modules/feed/services/post.mapper.ts
@@ -20,7 +20,7 @@ export class PostMapper {
 
     const youtubeRegex = /http[s]?:\/\/?www\.(?:youtube\.com\/\S*(?:(?:\/e(?:mbed))?\/|watch\/?\?(?:\S*?&?v\=))|youtu\.be\/)([a-zA-Z0-9_-]{6,11})/gmi;
     
-    const urlRegex = /((http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-]))/gmi;
+    const urlRegex = /((http|ftp|https):\/\/(?:[\w_-]+(?:(?:(?::[0-9]{1,5})?(\.[\w_-]+)+)?))(?:[\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-]))/gmi;
     
     //const userTagRegex = /(?:\s|^)(@[a-zA-Z0-9](?:[._-](?![._-])|[a-zA-Z0-9]){1,18}[a-zA-Z0-9])(?:\s|[\b]|$)/gmi;
     const userTagRegex = /(?:\B)(@[a-zA-Z0-9](?:[._-](?![._-])|[a-zA-Z0-9]){1,18}[a-zA-Z0-9])/gmi;
@@ -29,6 +29,9 @@ export class PostMapper {
 
     var mappedMessage = message
     const urlMatches = [...mappedMessage.matchAll(urlRegex)]
+    console.log(urlMatches);
+    console.log(message);
+    
     
     const uniqueUrlMatches = new Set(urlMatches.map(match => match[0]));
      
